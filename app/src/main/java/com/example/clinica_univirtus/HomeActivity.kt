b83a8ba3a397fbb1d6687d9dd293e07a9bd5550a
Amanda <amanda.profissional078@gmail.com>
2026-03-13 01:42:51 -0300

    Criação home e adição de icones

--- app/src/main/java/com/example/clinica_univirtus/HomeActivity.kt
+++ app/src/main/java/com/example/clinica_univirtus/HomeActivity.kt
@@ -1,28 +1,20 @@
 package com.example.clinica_univirtus
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class LoginActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_home)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val btn_entrar = findViewById<Button>(R.id.btn_entrar)
-        btn_entrar.setOnClickListener {
-            val intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
         }
     }
 }
