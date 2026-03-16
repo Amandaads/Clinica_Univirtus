3a5b92da46f4d969dca3c0f554846f35ed37b218
thiagobrazil <tandrade94@live.com>
2026-03-16 19:54:23 -0300

    Criação de fragmentos e implementação da navegação principal

--- app/src/main/java/com/example/clinica_univirtus/MainActivity.kt
+++ app/src/main/java/com/example/clinica_univirtus/MainActivity.kt
@@ -5,7 +5,13 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import com.example.clinica_univirtus.databinding.ActivityMainBinding
+import com.example.clinica_univirtus.fragments.AgendamentosFragment
+import com.example.clinica_univirtus.fragments.AgendarFragment
+import com.example.clinica_univirtus.fragments.ContatoFragment
+import com.example.clinica_univirtus.fragments.InicioFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,11 +19,18 @@ class MainActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val bottomNav = binding.menuNavegacao
+        ViewCompat.setOnApplyWindowInsetsListener(bottomNav) { view, insets ->
+            view.setPadding(0, 0, 0, 0)
+            insets
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -25,6 +38,26 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        trocarFragmento(InicioFragment())
 
+        binding.menuNavegacao.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.item_inicio -> trocarFragmento(InicioFragment())
+                R.id.item_agendar -> trocarFragmento(AgendarFragment())
+                R.id.item_agendamentos -> trocarFragmento(AgendamentosFragment())
+                R.id.item_contato -> trocarFragmento(ContatoFragment())
+                else -> {
+                    trocarFragmento(InicioFragment())
+                }
+            }
+            true
+        }
+    }
+
+    private fun trocarFragmento(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
     }
 }
